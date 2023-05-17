c4e560a0aee8ce866e7909a15da5cc7b608c4420
Sharifur <sharifurrahman@Sharifurs-MacBook-Pro.local>
2023-05-18 00:52:23 +0600

    ui change, plugin active/deactive, delete work done

--- Http/Controllers/PluginManageController.php
+++ Http/Controllers/PluginManageController.php
@@ -5,6 +5,8 @@ namespace Modules\PluginManage\Http\Controllers;
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\File;
+use Modules\PluginManage\Http\Helpers\PluginManageHelpers;
 
 class PluginManageController extends Controller
 {
@@ -14,7 +16,10 @@ class PluginManageController extends Controller
      */
     public function index()
     {
-        return view('pluginmanage::index');
+        //todo write a call to access all json content and update it
+        //todo return list of plugins devided by core and external
+        $pluginList = PluginManageHelpers::getPluginLists();
+        return view('pluginmanage::plugin-manage.index',compact("pluginList"));
     }
 
     /**
@@ -67,13 +72,25 @@ class PluginManageController extends Controller
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     * @param int $id
-     * @return Renderable
-     */
-    public function destroy($id)
+    public function delete_plugin(Request $request)
     {
-        //
+        $request->validate([
+           "plugin" => "required|string"
+        ]);
+        //todo:: remove this name from modules_status.json file
+        File::deleteDirectory(module_path(implode("",explode(" ",$request->plugin))));
+        return response()->json("ok");
+    }
+    public function change_status(Request $request)
+    {
+        $request->validate([
+           "plugin" => "required|string",
+           "status" => "required|string",
+        ]);
+        $status = $request->status == 1 ? false : true;
+        PluginManageHelpers::getPluginInfo(implode("",explode(" ",$request->plugin)))->changePluginStatus($status )->saveModuleListFile();
+//        dd($info);
+//        File::deleteDirectory(module_path(implode("",explode(" ",$request->plugin))));
+        return response()->json("ok");
     }
 }
--- Routes/web.php
+++ Routes/web.php
@@ -15,7 +15,10 @@
      LANDLORD ADMIN ROUTES
 -------------------------------------------- */
 Route::group(['middleware' => ['auth:admin','adminglobalVariable', 'set_lang'],'prefix' => 'admin-home'],function () {
-    Route::get("plugin-manage",[\Modules\PluginManage\Http\Controllers\PluginManageController::class,"index"])->name("landlord.plugin.manage");
+    Route::get("plugin-manage/all",[\Modules\PluginManage\Http\Controllers\PluginManageController::class,"index"])->name("landlord.plugin.manage.all");
+    Route::get("plugin-manage/new",[\Modules\PluginManage\Http\Controllers\PluginManageController::class,"add_new"])->name("landlord.plugin.manage.new");
+    Route::post("plugin-manage/delete",[\Modules\PluginManage\Http\Controllers\PluginManageController::class,"delete_plugin"])->name("landlord.plugin.manage.delete");
+    Route::post("plugin-manage/status",[\Modules\PluginManage\Http\Controllers\PluginManageController::class,"change_status"])->name("landlord.plugin.manage.status.change");
 });
 
 
